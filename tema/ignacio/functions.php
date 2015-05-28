8ba172216cb2cfce8c17d0ce0eddfb9e7025a622
Ignacio <ignacio@Z00.local>
2015-05-28 22:03:08 +0200

    Tema

--- tema/ignacio/functions.php
+++ tema/ignacio/functions.php
@@ -14,3 +14,32 @@ function ignacio_setup() {
 }
 add_action( 'after_setup_theme', 'ignacio_setup' );
 
+
+function ignacio_widgets_init() {
+	$args = array(
+		'name'          => __( 'Right Sidebar', 'ignacio' ),
+		'id'            => 'right-sidebar',
+		'description'   => __( 'The sidebar on the right', 'ignacio' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>'
+	);
+
+	register_sidebar( $args );	
+}
+
+
+add_action( 'widgets_init' , 'ignacio_widgets_init' );
+
+function ignacio_excerpt_length( $length ) {
+	return 5;
+}
+add_filter( 'excerpt_length', 'ignacio_excerpt_length' );
+
+function ignacio_excerpt_more( $more ) {
+	$link = get_permalink();
+	return sprintf( __( '<a href="%s">Read more...</a>', 'ignacio' ), $link );
+
+}
+add_filter( 'excerpt_more', 'ignacio_excerpt_more' );
--- tema/ignacio/index.php
+++ tema/ignacio/index.php
@@ -16,8 +16,45 @@
 		</div>
 
 		<div id="content">
-			<div id="inner-content"></div>		
-			<div id="sidebar"></div>
+			<div id="inner-content">
+				<?php if ( have_posts() ): ?>
+					<?php while ( have_posts() ): the_post(); ?>
+						<article id="id-<?php the_ID(); ?>" <?php post_class(); ?> >
+							<header class="post-header">
+
+								<?php if ( is_singular() ): ?>
+									<h1><?php the_title(); ?></h1>							
+								<?php else: ?>
+									<h2><a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>							
+								<?php endif; ?>
+								
+							</header>
+
+							<div class="post-content">
+								<?php if ( is_singular() ): ?>
+									<?php the_content(); ?>
+								<?php elseif ( ! is_search() ): ?>
+									<?php the_excerpt(); ?>
+								<?php endif; ?>
+							</div>
+
+							<footer class="post-footer">
+								<div class="post-category meta"><?php the_category( ' | ' ); ?></div>
+								<div class="post-category meta"><?php the_tags( ' | ' ); ?></div>
+								<?php edit_post_link(); ?><br>
+								<?php wp_link_pages(); ?><br>
+								<?php comments_popup_link(); ?><br>
+
+							</footer>
+						</article>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div>		
+			<div id="sidebar">
+				<?php if ( is_active_sidebar( 'right-sidebar' ) ): ?>
+					<?php dynamic_sidebar( 'right-sidebar' ); ?>
+				<?php endif; ?>
+			</div>
 		</div>
 
 		<div id="footer">
